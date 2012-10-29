021fa27f509cb25b520c174dbcec41eed27e65da
Vincent Endoma <endomav@gmail.com>
2012-10-29 06:05:10 -0700

    Sample XML File

--- src/main/java/com/seck/homework2/CrawlInfo.java
+++ src/main/java/com/seck/homework2/CrawlInfo.java
@@ -1,5 +1,14 @@
 package com.seck.homework2;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+
 public class CrawlInfo {
 
 	/**
@@ -7,7 +16,38 @@ public class CrawlInfo {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
+		SAXBuilder builder = new SAXBuilder();
+		File xmlFile = new File("Files/Sample.xml");
+				
+		try {
+			
+			Document document = (Document) builder.build(xmlFile);
+			
+			Element rootNode = document.getRootElement();
+
+			List list = rootNode.getChildren("site");
+			
+			for( int i = 0; i < list.size(); i++ ) {
+				
+				Element node = (Element) list.get(i);
+				
+				System.out.println(node.getChildText("url"));
+				
+			}
+		
+		
+		} catch (JDOMException jdomex) {
+			
+			System.out.println(jdomex.getMessage());
+		
+		} catch (IOException io) {
+			
+			System.out.println(io.getMessage());
+		
+		}
+		
 	}
 
 }
