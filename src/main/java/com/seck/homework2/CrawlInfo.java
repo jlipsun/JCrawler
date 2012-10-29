758f8bf977b138422f6267140808aab7ff92acfa
Vincent Endoma <endomav@gmail.com>
2012-10-29 05:05:30 -0700

    Homework 2 Setup
    Once you clone this start shell, do "mvn install", then "mvn
    eclipse:eclipse"

---
+++ src/main/java/com/seck/homework2/CrawlInfo.java
@@ -0,0 +1,13 @@
+package com.seck.homework2;
+
+public class CrawlInfo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/seck/homework2/CrawlerInfo.java
@@ -0,0 +1,13 @@
+package com.seck.homework2;
+
+public class CrawlerInfo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
